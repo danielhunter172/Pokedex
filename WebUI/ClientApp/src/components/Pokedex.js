3f2c646894664c67c3d798d6e857f441007bb9bb
Daniel <danielhunter172@hotmail.com>
2021-11-27 02:44:15 +1000

    Created Pokedex component

---
+++ WebUI/ClientApp/src/components/Pokedex.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export class Pokedex extends Component {
+    static displayName = Pokedex.name;
+
+    render () {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
