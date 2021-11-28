11910fec9824662172b874c0a60c9252ab135b2a
Daniel <danielhunter172@hotmail.com>
2021-11-29 08:38:33 +1000

    Created GetPokemonQuery
    Created GetPokemonQuery and called it in Controller

---
+++ Application/Pokedex/Queries/GetPokemonQuery.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+using Domain.Models;
+using Newtonsoft.Json.Linq;
+
+namespace Application.Pokedex.Queries
+{
+    public class GetPokemonQuery
+    {
+        public HttpClient _http;
+
+        public GetPokemonQuery(HttpClient http)
+        {
+            _http = http;
+        }
+        
+        public async Task<List<Pokemon>> Get()
+        {
+            var response = await _http.GetAsync("https://pokeapi.co/api/v2/pokemon?limit=100&offset=0");
+            var content = await response.Content.ReadAsStringAsync();
+            var json = JObject.Parse(content);
+
+            List<Pokemon> pokemonList = new List<Pokemon>();
+            int i = 0;
+            foreach (var pokemon in json["results"])
+            {
+                Pokemon poke = new Pokemon();
+                poke.Id = i + 1;
+                poke.Name = pokemon["name"].ToString();
+                poke.ImageLink = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/" + (i + 1) + ".png";
+                i++;
+                pokemonList.Add(poke);
+            }
+
+            return pokemonList;
+        }
+    }
+}
---
+++ WebUI/Controllers/PokedexController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Application.Pokedex.Queries;
+using Domain.Models;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using NuGet.Protocol;
+using WebUI.Pages;
+using JsonNamingPolicy = System.Text.Json.JsonNamingPolicy;
+using JsonSerializerOptions = System.Text.Json.JsonSerializerOptions;
+
+namespace WebUI.Controllers
+{
+    [Route("api/Pokedex")]
+    [ApiController]
+    public class PokedexController : ControllerBase
+    {
+        private readonly HttpClient _http;
+        private GetPokemonQuery _query;
+        public PokedexController()
+        {
+            _http = new HttpClient();
+            _query = new GetPokemonQuery(_http);
+        }
+        
+        [HttpGet]
+        public async Task<List<Pokemon>> GetPokedex()
+        {
+           return await _query.Get();
+        }
+    }
+}
