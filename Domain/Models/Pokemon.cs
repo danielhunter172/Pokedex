d20f00cc15d233f6d406e0aed010509628bbbe2e
Daniel <danielhunter172@hotmail.com>
2021-11-29 08:38:39 +1000

    Create Pokemon.cs

---
+++ Domain/Models/Pokemon.cs
@@ -0,0 +1,9 @@
+﻿namespace Domain.Models
+{
+    public class Pokemon
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public string ImageLink { get; set; }
+    }
+}
