15c7162644f29ca33fb80480052f12d62e3a45e7
Daniel <danielhunter172@hotmail.com>
2021-11-30 08:28:41 +1000

    Update Pokedex.js
    Reference Pokemon component

--- WebUI/ClientApp/src/components/Pokedex.js
+++ WebUI/ClientApp/src/components/Pokedex.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './Pokedex.css'
+import {Pokemon} from "./Pokemon";
 
 export class Pokedex extends Component {
     constructor(props) {
@@ -22,16 +23,29 @@ export class Pokedex extends Component {
 
     render () {
         return (
-            <div className="scroll card card-body bg-light col-2 offset-1 mt-5">
-                <ul className="list-group">
-                    {this.state.pokedex.map(pokemon => 
-                        <li className="list-group-item">
-                            <img src={pokemon.imageLink}/>
-                            {pokemon.id}. {pokemon.name}
-                        </li>
-                    )}
-                    </ul>
-            </div>
+            <div className="row">
+                <div className="col-4">
+                    <div className="scroll card card-body bg-light mt-5">
+                        <ul className="list-group">
+                            {this.state.pokedex.map(pokemon =>
+                                <li className="list-group-item">
+                                    <img src={pokemon.imageLink}/>
+                                    {pokemon.id}. {pokemon.name}
+                                </li>
+                            )}
+                        </ul>
+                    </div>
+                </div>
+                <div className="col-6">
+                    <div className="row">
+                        <div className="col-12">
+                            <div className="card card-body bg-light mt-5">
+                                <Pokemon/>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>            
         );
     }
 }
