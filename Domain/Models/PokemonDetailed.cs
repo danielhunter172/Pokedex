c6f50aa08923e83340251b4b1cdbc8de055baca5
Daniel <danielhunter172@hotmail.com>
2021-12-02 12:00:50 +1000

    Update PokemonDetailed.cs

--- Domain/Models/PokemonDetailed.cs
+++ Domain/Models/PokemonDetailed.cs
@@ -1,9 +1,22 @@
-﻿namespace Domain.Models
+﻿using Domain.Models.PokemonDetails;
+
+namespace Domain.Models
 {
     public class PokemonDetailed
     {
-        public long Id { get; set; }
-        public string Name { get; set; }
-        public string ImageLink { get; set; }
+        public long id { get; set; }
+        public string name { get; set; }
+        public int capture_rate { get; set; }
+        public int base_experience { get; set; }
+        public int height { get; set; }
+        public int weight { get; set; }
+        public string location_area_encounters { get; set; }
+        public Stat[] stats { get; set; }
+        public string[] abilities { get; set; }
+        public string[] moves { get; set; }
+        public string[] sprites { get; set; }
+        public string[] egg_groups { get; set; }
+        public string description { get; set; }
+        public string growth_rate { get; set; }
     }
 }
