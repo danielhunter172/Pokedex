1ee91d119d368bc2d98087f3c491aecccef8ccc4
Daniel <danielhunter172@hotmail.com>
2021-12-03 13:54:04 +1000

    Called get(id)

--- WebUI/ClientApp/src/components/Pokemon.js
+++ WebUI/ClientApp/src/components/Pokemon.js
@@ -3,8 +3,17 @@ import React, { Component } from "react";
 export class Pokemon extends Component {
     constructor(props) {
         super(props);
+        
+        this.state = { pokemon: [] }
     }
     
+    async componentDidMount() {
+        let response = await fetch('https://localhost:7062/api/Pokedex/1');
+        let json = await response.json();
+        this.setState({ pokemon: json });
+        console.log(this.state.pokemon);
+    }
+
     render() {
         return(
             <div>
