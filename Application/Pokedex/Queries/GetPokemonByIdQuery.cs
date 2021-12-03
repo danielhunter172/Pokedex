77d06152efa4ec35c610af6b9a636ab5c58d76bb
Daniel <danielhunter172@hotmail.com>
2021-12-03 13:54:30 +1000

    Refractoring

--- Application/Pokedex/Queries/GetPokemonByIdQuery.cs
+++ Application/Pokedex/Queries/GetPokemonByIdQuery.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.Collections.Immutable;
+using System.Diagnostics;
 using System.Net.Http;
 using System.Threading.Tasks;
 using Domain.Models;
@@ -21,61 +22,54 @@ namespace Application.Pokedex.Queries
         {
             var response = await _http.GetAsync("https://pokeapi.co/api/v2/pokemon/" + id);
             var content = await response.Content.ReadAsStringAsync();
-            var json = JObject.Parse(content);
+            var jsonPoke = JObject.Parse(content);
 
-            var data = json["result"];
-            
             response = await _http.GetAsync("https://pokeapi.co/api/v2/pokemon-species/" + id);
             content = await response.Content.ReadAsStringAsync();
-            json = JObject.Parse(content);
+            var jsonSpec = JObject.Parse(content);
 
-            var dataSpecies = json["result"];
-            
             PokemonDetailed pokemon = new PokemonDetailed()
             {
-                id = data["id"].ToObject<long>(),
-                name = data["name"].ToString(),
-                base_experience = data["base_experience"].ToObject<int>(),
-                height = data["height"].ToObject<int>(),
-                weight = data["weight"].ToObject<int>(),
-                location_area_encounters = data["location_area_encounters"].ToString(),
-                sprite = data["sprites"]["front_default"].ToString(),
-                
-                capture_rate = dataSpecies["capture_rate"].ToObject<int>(),
-                description = dataSpecies["flavor_text_entries"][0]["flavor_text"].ToString(),
-                growth_rate = dataSpecies["growth_rate"]["name"].ToString()
-            };
+                id = jsonPoke["id"].ToObject<long>(),
+                name = jsonPoke["name"].ToString(),
+                base_experience = jsonPoke["base_experience"].ToObject<int>(),
+                height = jsonPoke["height"].ToObject<int>(),
+                weight = jsonPoke["weight"].ToObject<int>(),
+                location_area_encounters = jsonPoke["location_area_encounters"].ToString(),
+                sprite = jsonPoke["sprites"]["front_default"].ToString(),
 
-            int i = 0;
-            foreach (var stat in data["stats"])
+                capture_rate = jsonSpec["capture_rate"].ToObject<int>(),
+                description = jsonSpec["flavor_text_entries"][0]["flavor_text"].ToString(),
+                growth_rate = jsonSpec["growth_rate"]["name"].ToString(),
+                
+                stats = new List<Stat>(),
+                abilities = new List<string>(),
+                moves = new List<string>(),
+                egg_groups = new List<string>()
+            };
+            
+            foreach (var stat in jsonPoke["stats"])
             {
-                pokemon.stats[i] = new Stat()
+                pokemon.stats.Add(new Stat()
                 {
                     base_stat = stat["base_stat"].ToObject<int>(),
                     name = stat["stat"]["name"].ToString()
-                };
-                i++;
+                });
             }
 
-            i = 0;
-            foreach (var ability in data["abilities"])
+            foreach (var ability in jsonPoke["abilities"])
             {
-                pokemon.abilities[i] = ability["ability"]["name"].ToString();
-                i++;
+                pokemon.abilities.Add(ability["ability"]["name"].ToString());
             }
 
-            i = 0;
-            foreach (var move in data["moves"])
+            foreach (var move in jsonPoke["moves"])
             {
-                pokemon.moves[i] = move["move"]["name"].ToString();
-                i++;
+                pokemon.moves.Add(move["move"]["name"].ToString());
             }
 
-            i = 0;
-            foreach (var group in dataSpecies["egg_groups"])
+            foreach (var group in jsonSpec["egg_groups"])
             {
-                pokemon.egg_groups[i] = group["name"].ToString();
-                i++;
+                pokemon.egg_groups.Add(group["name"].ToString());
             }
             
             return pokemon;
