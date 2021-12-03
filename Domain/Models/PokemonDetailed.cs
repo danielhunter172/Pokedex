cd7e4b58d79bdb02c37fef075f55a737042b6c74
Daniel <danielhunter172@hotmail.com>
2021-12-03 13:54:18 +1000

    Made properties nullable

--- Domain/Models/PokemonDetailed.cs
+++ Domain/Models/PokemonDetailed.cs
@@ -1,22 +1,23 @@
-﻿using Domain.Models.PokemonDetails;
+﻿using System.Collections.Generic;
+using Domain.Models.PokemonDetails;
 
 namespace Domain.Models
 {
     public class PokemonDetailed
     {
         public long id { get; set; }
-        public string name { get; set; }
-        public int capture_rate { get; set; }
-        public int base_experience { get; set; }
-        public int height { get; set; }
-        public int weight { get; set; }
-        public string location_area_encounters { get; set; }
-        public Stat[] stats { get; set; }
-        public string[] abilities { get; set; }
-        public string[] moves { get; set; }
-        public string sprite { get; set; }
-        public string[] egg_groups { get; set; }
-        public string description { get; set; }
-        public string growth_rate { get; set; }
+        public string? name { get; set; }
+        public int? capture_rate { get; set; }
+        public int? base_experience { get; set; }
+        public int? height { get; set; }
+        public int? weight { get; set; }
+        public string? location_area_encounters { get; set; }
+        public List<Stat>? stats { get; set; }
+        public List<string>? abilities { get; set; }
+        public List<string>? moves { get; set; }
+        public string? sprite { get; set; }
+        public List<string>? egg_groups { get; set; }
+        public string? description { get; set; }
+        public string? growth_rate { get; set; }
     }
 }
