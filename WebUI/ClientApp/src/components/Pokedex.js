6667b2ed3b879fbc3c9f3c0cf8e4f51ef710b463
Daniel <danielhunter172@hotmail.com>
2021-12-04 06:02:52 +1000

    Styling

--- WebUI/ClientApp/src/components/Pokedex.js
+++ WebUI/ClientApp/src/components/Pokedex.js
@@ -7,10 +7,11 @@ export class Pokedex extends Component {
         super(props);
         this.state = { 
             pokedex: [],
-            selectedId: 1
+            selectedId: 0
         }
     }
     static displayName = Pokedex.name;
+    pokemonDiv = <div></div>;
     
     async getPokedex () {
         let response = await fetch('https://localhost:7062/api/Pokedex');
@@ -26,10 +27,13 @@ export class Pokedex extends Component {
         if (id !== this.state.selectedId)
         {
             this.setState({ selectedId: id });
+            this.pokemonDiv = <Pokemon id={this.state.selectedId}/>;
         }
     }
 
     render () {
+        
+        
         return (
             <div className="row">
                 <div className="col-4">
@@ -49,7 +53,7 @@ export class Pokedex extends Component {
                 <div className="col-6">
                     <div className="row">
                         <div className="col-12">
-                            <div className="card card-body bg-light mt-5">
+                            <div className="card bg-light mt-5">
                                 <Pokemon id={this.state.selectedId}/>
                             </div>
                         </div>
--- WebUI/ClientApp/src/components/Pokemon.js
+++ WebUI/ClientApp/src/components/Pokemon.js
@@ -1,5 +1,26 @@
 import React, { Component } from "react";
 
+const TYPE_COLORS = {
+    bug: 'B1C12E',
+    dark: '4F3A2D',
+    dragon: '755EDF',
+    electric: 'FCBC17',
+    fairy: 'F4B1F4',
+    fighting: '823551',
+    fire: 'E73B0C',
+    flying: 'A3B3F7',
+    ghost: '6060B2',
+    grass: '74C236',
+    ground: 'D3B357',
+    ice: 'A3E7FD',
+    normal: 'C8C4BC',
+    poison: '934594',
+    psychic: 'ED4882',
+    rock: 'B9A156',
+    steel: 'B5B5C3',
+    water: '3295F6'
+};
+
 export class Pokemon extends Component {
     constructor(props) {
         super(props);
@@ -9,18 +30,37 @@ export class Pokemon extends Component {
     
     async componentDidUpdate(prevProps) {
         if (prevProps.id !== this.props.id) {
+            this.setState({ pokemon: [] });
             let response = await fetch('https://localhost:7062/api/Pokedex/' + this.props.id);
             let json = await response.json();
             this.setState({ pokemon: json });
-            console.log(this.state.pokemon);
+            
         }
     }
 
     render() {
-        return(
+        const {pokemon} = this.state;
+        
+        return pokemon.id === undefined ? <span>Loading...</span> : (
             <div>
-                <img src={this.state.pokemon["sprite"]}/>
+                <div className="card-header">
+                    {this.state.pokemon.id}. {this.state.pokemon.name}
+                    <div className="float-end">{this.state.pokemon["types"].map(type => 
+                        <span key={type} 
+                              className="badge badge-pill mr-1" 
+                              style={{
+                                  backgroundColor: `#${TYPE_COLORS[type]}`,
+                                  color: 'white'
+                              }}>
+                            {type.toUpperCase()}                        
+                        </span>
+                    )}
+                    </div>
+                </div>
+                <div className="card-body">
+                    <img src={this.state.pokemon.sprite}/>
+                </div>
             </div>
-        )
+        )        
     }
 }
