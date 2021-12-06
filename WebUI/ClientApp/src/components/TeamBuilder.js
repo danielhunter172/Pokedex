4f1341c7fc0ea1d18e8ff156dca00cbae96d2aae
Daniel <danielhunter172@hotmail.com>
2021-12-07 08:04:38 +1000

    Created TeamBuilder and Team components
    Added boilerplate code

---
+++ WebUI/ClientApp/src/components/TeamBuilder.js
@@ -0,0 +1,27 @@
+﻿import React, {Component} from "react";
+import {PokemonList} from "./PokemonList";
+
+export class TeamBuilder extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render () {
+        return (
+            <div className="row">
+                <div className="offset-1 col-3">
+                    <PokemonList/>
+                </div>
+                <div className="col-6">
+                    <div className="row">
+                        <div className="col-12">
+                            <div className="card bg-light mt-5">
+                                
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
