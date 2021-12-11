f5ddc0868d9a5fc4e8b2db1958c0273033bacf6f
Daniel <danielhunter172@hotmail.com>
2021-12-11 10:34:23 +1000

    Create TeamPokemon model

---
+++ Domain/Models/TeamPokemon.cs
@@ -0,0 +1,15 @@
+﻿namespace Domain.Models
+{
+    public class TeamPokemon
+    {
+        public int Id { get; set; }
+        public int? PokemonId { get; set; }
+        public string? Name { get; set; }
+        public string? Type1 { get; set; }
+        public string? Type2 { get; set; }
+        public string? Move1 { get; set; }
+        public string? Move2 { get; set; }
+        public string? Move3 { get; set; }
+        public string? Move4 { get; set; }
+    }
+}
