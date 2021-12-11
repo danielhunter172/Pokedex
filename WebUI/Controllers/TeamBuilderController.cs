44b7cde6fcdbf49192f14aac4098752ff5339732
Daniel <danielhunter172@hotmail.com>
2021-12-11 10:36:57 +1000

    Create TeamBuilderController.cs

---
+++ WebUI/Controllers/TeamBuilderController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Domain.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebUI.Controllers
+{
+    [Route("api/TeamBuilder")]
+    [ApiController]
+    public class TeamBuilderController : ControllerBase
+    {
+        private readonly TeamContext _context;
+
+        public TeamBuilderController(TeamContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<List<TeamPokemon>> GetTeam()
+        {
+            return await _context.PokemonTeam.ToListAsync();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<TeamPokemon>> PostPokemon(TeamPokemon pokemon)
+        {
+            _context.Add(pokemon);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetTeam), new {id = pokemon.Id}, pokemon);
+        }
+    }
+}
