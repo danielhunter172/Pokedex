c6a2cb5079d24ae8340b1e65e88e79bc69384c30
Daniel <danielhunter172@hotmail.com>
2021-12-11 10:36:24 +1000

    Created Context for TeamPokemon

---
+++ Domain/Models/TeamContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Domain.Models
+{
+    public class TeamContext : DbContext
+    {
+        public TeamContext(DbContextOptions<TeamContext> options)
+            : base(options)
+        {
+            
+        }
+
+        public DbSet<TeamPokemon> PokemonTeam { get; set; } = null!;
+    }
+}
