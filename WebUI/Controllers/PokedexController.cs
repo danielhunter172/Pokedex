892ca48fba22d65069ad96d27c70d45584b9cece
Daniel <danielhunter172@hotmail.com>
2021-12-11 10:35:31 +1000

    Removed unused imports

--- WebUI/Controllers/PokedexController.cs
+++ WebUI/Controllers/PokedexController.cs
@@ -1,18 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.Net.Http;
 using System.Threading.Tasks;
 using Application.Pokedex.Queries;
 using Domain.Models;
-using MediatR;
-using Microsoft.AspNetCore.Mvc;
-using Newtonsoft.Json;
-using Newtonsoft.Json.Linq;
-using NuGet.Protocol;
-using WebUI.Pages;
-using JsonNamingPolicy = System.Text.Json.JsonNamingPolicy;
-using JsonSerializerOptions = System.Text.Json.JsonSerializerOptions;
+using Microsoft.AspNetCore.Mvc; 
 
 namespace WebUI.Controllers
 {
